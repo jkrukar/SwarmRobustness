77af41f26085c365d1fa6a8be1df093211dbeca0
John Krukar <john.a.krukar@gmail.com>
2017-10-30 17:12:27 -0600

    Add files via upload

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 rm -rf build
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DARGOS_BUILD_NATIVE=ON .. && make
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-std=c++11" .. && make
 
